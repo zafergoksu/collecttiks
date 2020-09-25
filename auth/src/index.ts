6ca80db6f15ca71f53ab1ba7052435f9bd37edf8
zgoksu <zafergoksu00@gmail.com>
2020-09-25 19:17:34 +0100

    gcloud implementation and auth normalisation methods

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,30 @@
 import express from 'express';
+import 'express-async-errors';
+
+import currentUserRouter from './routes/current-user';
+import signinRouter from './routes/signin';
+import signoutRouter from './routes/signout';
+import signupRouter from './routes/signup';
+import errorHandler from './middleware/error-handler';
+import NotFoundError from './errors/not-found-error';
 
 const app = express();
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send("Hi there");
+// Route handlers
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
+
+// Other routes get precidence to route string paths, the glob modifier captures everything else
+// that fails
+app.all('*', async (req, res) => {
+    //const err = new NotFoundError();
+    //res.status(err.statusCode).send(err.serializeErrors());
+    throw new NotFoundError();
 });
 
 app.listen(3000, () => {
