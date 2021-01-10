792f50b22ffdf4cff3af8e22b33ee57fec6184aa
zgoksu <zafergoksu00@gmail.com>
2021-01-10 19:09:53 +0000

    Revert "Fixed minikube cross namespace service communication"
    This reverts commit 6371286d3a43f049e61ae93e3309edf474fca72e.

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,9 +4,8 @@ export default ({ req }) => {
     if (typeof window === 'undefined') {
         // On the server
         return axios.create({
-            baseURL:
-                'http://ingress-nginx-controller.kube-system.svc.cluster.local',
-            headers: req.headers,
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            headers: req.headers
         });
     } else {
         // On the client
