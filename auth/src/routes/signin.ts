8a97e5eed40d3516aed6a0c3fe674e795e014dce
zgoksu <zafergoksu00@gmail.com>
2021-01-17 16:27:21 +0000

    Fix formatting on signin.ts within auth

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { validateRequest, BadRequestError } from '@zgoksutickets/common-utils'
+import { validateRequest, BadRequestError } from '@zgoksutickets/common-utils';
 
 import { Password } from '../util/password';
 import User from '../models/user';
