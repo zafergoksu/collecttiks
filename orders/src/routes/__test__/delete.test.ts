5d3d9d64cd5af29a338b0c1e5594be5f0d2564ef
zgoksu <zafergoksu00@gmail.com>
2021-02-06 19:16:25 +0000

    Add and test delete.ts, to mark orders as cancelled

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,37 @@
+import { OrderStatus } from '@zgoksutickets/common-utils';
+import request from 'supertest';
+import app from '../../app';
+import Order from '../../models/orders';
+import Ticket from '../../models/ticket';
+
+describe('delete.ts', () => {
+    it('should mark an order as cancelled', async () => {
+        // Create a ticket with Ticket model
+        const ticket = Ticket.build({
+            title: 'concert',
+            price: 20,
+        });
+        await ticket.save();
+
+        // Make a request to create an order
+        const user = global.signin();
+
+        const order = await request(app)
+            .post('/api/orders')
+            .set('Cookie', user)
+            .send({ ticketId: ticket.id })
+            .expect(201);
+
+        // Make a request to cancel the order
+        await request(app)
+            .delete(`/api/orders/${order.body.id}`)
+            .set('Cookie', user)
+            .expect(204);
+
+        // Expect the order to be cancelled
+        const updatedOrder = await Order.findById(order.body.id);
+        expect(updatedOrder!.status).toEqual(OrderStatus.CANCELLED);
+    });
+
+    it.todo('emits an order cancelled event');
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
+import {
+    NotAuthorizedError,
+    NotFoundError,
+    OrderStatus,
+} from '@zgoksutickets/common-utils';
 import express, { Request, Response } from 'express';
+import Order from '../models/orders';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({});
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.CANCELLED;
+    await order.save();
+
+    // Publishing an event saying this was cancelled
+
+    res.status(204).send(order);
 });
 
 export default router;
