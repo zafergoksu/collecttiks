7c2e7e10d0d8704f1bfe93f04465d1f193f5b0b4
zgoksu <zafergoksu00@gmail.com>
2021-04-26 01:00:45 +0100

    Add test for optimistic concurrency control

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
     title: string;
@@ -6,10 +7,13 @@ interface TicketAttrs {
     userId: string;
 }
 
+// List out all of the properties an instance of a ticket document
+// can have.
 interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -40,6 +44,9 @@ const ticketSchema = new mongoose.Schema(
         },
     }
 );
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
