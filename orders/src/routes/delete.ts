9fe3d4b02d39a62f623b4ba2b2df5b19dd4af794
zgoksu <zafergoksu00@gmail.com>
2021-07-04 18:53:00 +0100

    Fixes

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -28,7 +28,7 @@ router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     // Publishing an event saying this was cancelled
     new OrderCancelledPublisher(natsWrapper.client).publish({
         id: order.id,
-        version: order.__v!,
+        version: order.version,
         ticket: {
             id: order.ticket.id,
         },
