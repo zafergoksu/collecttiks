71681abdfa62413fe56cf6cdf0f93f5710f63bd3
Zafer Goksu <zafergoksu00@gmail.com>
2021-08-13 00:38:28 +0100

    Remove nats-test

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from '@zgoksutickets/common-utils';
-import { TicketCreatedEvent } from '@zgoksutickets/common-utils';
-import { Subjects } from '@zgoksutickets/common-utils';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
