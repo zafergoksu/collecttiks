844426ca3adee3df6905f99bc49d558f64199923
zgoksu <zafergoksu00@gmail.com>
2021-08-16 19:59:11 +0100

    Update hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const buildClient = ({ req }) => {
     if (typeof window === 'undefined') {
         // On the server
         return axios.create({
-            baseURL:
-                'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.collecttiks.xyz',
             headers: req.headers,
         });
     } else {
