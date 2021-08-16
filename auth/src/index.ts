b40604022e74da1846b3b09dc47fa92868e384ea
zgoksu <zafergoksu00@gmail.com>
2021-08-16 17:01:59 +0100

    Update formatting

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -14,14 +14,14 @@ const start = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            useCreateIndex: true,
         });
     } catch (err) {
         console.log(err);
     }
 
     app.listen(3000, () => {
-        console.log("Listening on port 3000");
+        console.log('Listening on port 3000');
     });
 };
 
